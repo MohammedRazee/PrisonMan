2cfdb7ae220c2eb5707874a2558f690570dcd74a
Mohammed Razee <razeeismail15@gmail.com>
2025-05-30 20:14:07 +0530

    Bug Fixes

--- Prisonman/src/main/java/com/Prisonman/Prisonman/Repository/CellRepository.java
+++ Prisonman/src/main/java/com/Prisonman/Prisonman/Repository/CellRepository.java
@@ -3,7 +3,5 @@ package com.Prisonman.Prisonman.Repository;
 import com.Prisonman.Prisonman.Model.Cell;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
-
 public interface CellRepository extends MongoRepository<Cell, String> {
 }
